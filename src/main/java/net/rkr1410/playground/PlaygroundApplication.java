e679bdd3e59df07eaadadc1123209530a5fe42d6
Rafal Krzyzanowski <rkr1410@gmail.com>
2022-04-04 00:43:26 +0200

    Generate Initializr stuff

---
+++ src/main/java/net/rkr1410/playground/PlaygroundApplication.java
@@ -0,0 +1,13 @@
+package net.rkr1410.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/rkr1410/playground/PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package net.rkr1410.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
